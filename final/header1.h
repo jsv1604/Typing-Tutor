f47357c5f870ededf3bc25820c67f4044fe7820e
Jeffrey S Varghese <jeff0716042@gmail.com>
2022-08-31 00:30:04 +0530

    Final commit in this folder

---
+++ final/header1.h
@@ -0,0 +1,13 @@
+int menu(char name[]);
+int choice_selection(char name[],int choice);
+void starttyping(char name[]);
+void update_progresscard(char name[],int level,int score,int speed,float accuracy);
+void progresscard(char name[]);
+void rules(char name[]);
+void aboutus(char name[]);
+void endgame();
+
+void gotoxy(int x,int y);
+void delay(unsigned int mseconds);
+void setcolor(int ForgC);
+void sett(char name[]);
